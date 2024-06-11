8592489ffcde03ca47e91d52a0a7f35f444e3f90
Kisoon Kwon <moxie2ks@gmail.com>
2024-06-11 22:22:45 +0900

    Youtube 라이브 페이지 문구 및 스타일 변경

--- src/components/Youtube.svelte
+++ src/components/Youtube.svelte
@@ -1,29 +1,37 @@
 <script lang="ts">
-    const 문구 = `두 사람이 함께하는 여덟 번째 봄,
-    설레었던 첫 봄날의 마음을 떠올리며
-    평생의 짝이 되어 살아가려 합니다.
-    우리의 앞날을 약속하는 따뜻한 봄날
-    시간을 내주셔서 자리를 빛내어 주시면
-    더없는 기쁨으로 간직하겠습니다.`;
+    const 문구 = `참석이 어려우신 분들 께서는
+    온라인 중계로 시청하실 수 있습니다.`;
 </script>
     
-<section class="flex-center">
-    <h2 class="sectence">결혼식 유튜브 라이브</h2>
-</section>
+<section class="youtube-title flex-center">라이브 웨딩 안내</section>
+<section class="youtube-txt flex-center">{문구}</section>
+<section class="youtube-date flex-center">2024년 9월 7일 오후 1시 50분</section>
 <div class="video-wrap">
     <iframe width="560" height="315" src="https://www.youtube.com/embed/1WEsiSxhzBQ?si=a9LpACpt-gMtddma" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 </div>
 
 <style>
-.sectence {
+.youtube-title {
+    position: relative;
+    padding-top: 2.938rem;
+    margin-bottom: 2.5rem;
+    font-family: 'KoPub Batang';
+    font-size: 1rem;
+    font-weight: 700;
+    line-height: 1.250rem;
     text-align: center;
-    white-space: pre;
-    font-size: 1.2rem;
-    line-height: 2;
-    color: #333;
+    color: #111;
+    letter-spacing: 0.4em;
 }
-.video-wrap {position:relative; padding-bottom:56.25%; height:0; margin:3% ; overflow:hidden;}
-.video-wrap iframe,
-.video-wrap object,
-.video-wrap embed {position:absolute; top:0; left:0; width:100%; height:100%;}
+.youtube-txt{
+    margin-bottom:2.813rem;
+    font-size:0.813rem;
+    font-weight:300;
+    line-height:1.5rem;
+    color:#111;
+    text-align:center;
+    white-space:pre-wrap;
+}
+.video-wrap {position:relative; padding-bottom:56.25%; height:0; margin:3% ; overflow:hidden; margin-top:10%}
+.video-wrap iframe {position:absolute; top:0; left:0; width:100%; height:100%;}
 </style>
