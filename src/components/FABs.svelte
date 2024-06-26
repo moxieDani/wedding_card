e0f59fdd057a74a9f24260710a50b95e55463fdd
Kisoon Kwon <moxie2ks@gmail.com>
2024-06-26 18:27:11 +0900

    배경음악이 모바일에서 나오지 않은 문제 수정 + 페이지 최상위 보기 버튼 제거

--- src/components/FABs.svelte
+++ src/components/FABs.svelte
@@ -1,52 +1,23 @@
 <script lang="ts">
 import guitar from '@/assets/guitar.png';
-import upArrow from '@/assets/up-arrow.png';
 
-let audioEl: HTMLAudioElement;
-let audioElLoading: 'yet' | 'loading' | 'loaded' = 'yet';
+let audioPlayer: HTMLAudioElement | null = null;
 
-const loadAudio = () => {
-  return new Promise<void>((resolve, reject) => {
-    audioEl = document.createElement('audio');
-    audioEl.src = 'https://kr.object.ncloudstorage.com/wedding/music.mp3';
-    audioEl.preload = 'auto';
-    audioEl.volume = 0.2;
-    audioEl.autoplay = true;
-    audioEl.oncanplay = () => resolve();
-    audioEl.onerror = err => reject(err);
-  });
-};
+function playMusic() {
+  if (!audioPlayer) {
+    audioPlayer = new Audio('https://kr.object.ncloudstorage.com/wedding/music.mp3');
+  }
 
-const onToggleMusic = async () => {
-  if (audioElLoading === 'yet') {
-    audioElLoading = 'loading';
-    try {
-      await loadAudio();
-      audioElLoading = 'loaded';
-    } catch (error) {
-      console.error(error);
-      audioElLoading = 'yet';
-    }
-    return;
+  if (audioPlayer.paused) {
+    audioPlayer.play();
+  } else {
+    audioPlayer.pause();
   }
-  if (audioElLoading === 'loading') {
-    return console.warn('loading...');
-  }
-  if (audioElLoading === 'loaded') {
-    return audioEl.paused ? audioEl.play() : audioEl.pause();
-  }
-};
-const onClickUp = () => {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-};
+}
 </script>
 
-<button class="music-button" on:click={onToggleMusic}>
+<button class="music-button" on:click={playMusic}>
   <img src={guitar} alt="music-icon" />
-</button>
-
-<button class="up-button" on:click={onClickUp}>
-  <img src={upArrow} alt="up-icon" />
 </button>
 
 <style>
@@ -60,13 +31,5 @@ button {
 }
 .music-button img {
   width: 2rem;
-}
-.up-button {
-  position: fixed;
-  right: 0.5rem;
-  bottom: 0.5rem;
-}
-.up-button img {
-  width: 1.5rem;
 }
 </style>
