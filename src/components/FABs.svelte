e6f8d280f1ad139ae4ab2fe3a3145a041a1b82e1
Kisoon Kwon <ks.kwon@bitnine.net>
2024-07-15 12:54:11 +0900

    페이지가 로드되면 배경음악이 자동으로 재생되도록 수정

--- src/components/FABs.svelte
+++ src/components/FABs.svelte
@@ -1,35 +1,55 @@
 <script lang="ts">
-import guitar from '@/assets/guitar.png';
-
 let audioPlayer: HTMLAudioElement | null = null;
-
+let isPlaying: boolean = false;
+let isPageActive: boolean = true;
+let musicButtonStr = '🔇';
 function playMusic() {
   if (!audioPlayer) {
     audioPlayer = new Audio('https://kr.object.ncloudstorage.com/wedding/music.mp3');
-  }
-
-  if (audioPlayer.paused) {
     audioPlayer.play();
+    isPlaying = true;
+    musicButtonStr = '🔊';
+  } else if (audioPlayer.paused) {
+    audioPlayer.play();
+    isPlaying = true;
+    musicButtonStr = '🔊';
   } else {
     audioPlayer.pause();
+    isPlaying = false;
+    musicButtonStr = '🔇';
   }
 }
+
+// 페이지가 로드되면 자동으로 음악 재생
+window.addEventListener('load', playMusic);
+
+// 페이지 활성화 상태 추적
+window.addEventListener('focus', () => {
+  isPageActive = true;
+  if (isPlaying) {
+    audioPlayer?.play();
+    musicButtonStr = '🔊';
+  } else {
+    musicButtonStr = '🔇';
+  }
+});
+
+window.addEventListener('blur', () => {
+  isPageActive = false;
+  audioPlayer?.pause();
+  musicButtonStr = '🔇';
+});
 </script>
 
 <button class="music-button" on:click={playMusic}>
-  <img src={guitar} alt="music-icon" />
+  {musicButtonStr}
 </button>
 
 <style>
-button {
-  padding: 0.5rem;
-}
 .music-button {
+  padding: 0.5rem;
   position: fixed;
   left: 0.5rem;
   bottom: 0.5rem;
-}
-.music-button img {
-  width: 2rem;
 }
 </style>
